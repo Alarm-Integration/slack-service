7a025c22f151eb0eccc06b21d93aa0fe00c660ab
Root(김명석) <mskim@gabia.com>
2021-07-07 17:12:08 +0900

    add: slack sdk 추가 및 slack 실제 테스트

--- src/test/java/com/gabia/slack/SlackApplicationTests.java
+++ src/test/java/com/gabia/slack/SlackApplicationTests.java
@@ -1,13 +1,21 @@
 package com.gabia.slack;
 
+import com.slack.api.Slack;
+import com.slack.api.methods.MethodsClient;
+import com.slack.api.methods.SlackApiException;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.io.IOException;
+
 class SlackApplicationTests {
 
     @Test
-    void contextLoads() {
+    void 슬랙_전송_실제_테스트() throws SlackApiException, IOException {
+//        MethodsClient client = Slack.getInstance().methods();
+//
+//        client.chatPostMessage(r ->
+//                r.token("xoxb-2148325514801-2142207279172-ttsneJk3GUgXqkw3dtPPK5bS")
+//                        .channel("C023WJKCPUM")
+//                        .text("Hello"));
     }
-
 }
---
+++ src/test/java/com/gabia/slack/service/ChannelServiceTests.java
@@ -0,0 +1,32 @@
+package com.gabia.slack.service;
+
+import com.slack.api.Slack;
+import com.slack.api.methods.MethodsClient;
+import com.slack.api.methods.SlackApiException;
+import com.slack.api.methods.response.conversations.ConversationsListResponse;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class ChannelServiceTests {
+
+    @Test
+    void 슬랙_채널_받아오기_정상_토큰_결과_테스트() throws SlackApiException, IOException {
+        String accessToken = "xoxb-2148325514801-2142207279172-ttsneJk3GUgXqkw3dtPPK5bS";
+
+        MethodsClient client = Slack.getInstance().methods();
+        ConversationsListResponse conversationsListResponse = client.conversationsList(r -> r.token(accessToken));
+
+        System.out.println(conversationsListResponse);
+    }
+
+    @Test
+    void 슬랙_채널_받아오기_비정상_토큰_결과_테스트() throws SlackApiException, IOException {
+        String accessToken = "not-a-token";
+
+        MethodsClient client = Slack.getInstance().methods();
+        ConversationsListResponse conversationsList = client.conversationsList(r -> r.token(accessToken));
+
+        System.out.println(conversationsList);
+    }
+}
